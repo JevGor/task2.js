0cae161f67b18efbdc59c2a63a8eed64507a21f1
JevGor <reawqk@mail.ru>
2019-02-22 22:07:45 +0100

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,9 +1,8 @@
 var x  =  30, y  =  20;
-function change() {
-    x  =  x + y - x; 
-    y  =  y + x - y;
-}
 
+    x  =  x + y;
+    y  =  x - y;
+    x  =  x - y;
 
 console.log(x);
 console.log(y);
